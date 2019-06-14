b2e13a11ca48e8979702f87c9ec5d38ff0ea4b14
thecreamedcorn <wgaboury@gmail.com>
2019-06-13 21:28:34 -0400

    utilities for the all the unit tests

---
+++ src/test/java/io/github/thecreamedcorn/TestUtils.java
@@ -0,0 +1,10 @@
+package io.github.thecreamedcorn;
+
+import java.io.File;
+
+public class TestUtils {
+    public static void setupDatabase() {
+        File homeDir = new File(System.getenv("user.home"));
+
+    }
+}
